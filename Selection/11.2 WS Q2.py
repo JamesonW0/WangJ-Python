141a81146b6962e6eb7f71da5930f37e29b83220
JamesonW0 <51969225+JamesonW0@users.noreply.github.com>
2021-09-29 10:32:02 +0100

    Update 11.2 WS Q2.py

--- Selection/11.2 WS Q2.py
+++ Selection/11.2 WS Q2.py
@@ -1,8 +1,7 @@
 age = int(input('enter age'))
 
-if age < 11:
-    print('Invalid input: enter a value from 11 to 18')
-elif age > 18:
+if age < 11 or age > 18:
     print('Invalid input: enter a value from 11 to 18')
 else:
     print('Valid pupil age')
+# end if
