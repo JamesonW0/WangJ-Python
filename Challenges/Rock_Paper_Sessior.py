f9147c7a8f11d76e444038f06133e784122c56c2
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-10-04 21:01:39 +0100

    Feedback: Rock Paper Scissors
    This is a great solution and met all the brief, including no if's. What if I add, no dictionaries or 2D Lists...

--- Challenges/Rock_Paper_Sessior.py
+++ Challenges/Rock_Paper_Sessior.py
@@ -2,6 +2,7 @@ import random
 
 play = input('Do you want to play? (y/n)')
 options = ['Rock', 'Paper', 'Scissor']
+### SRC - This is a great solution, but can you do this with just a list ['Draw','You win', 'You Lose']?
 result = {'-2': 'You win', '-1': 'You lose', '0': 'Draw', '1': 'You win', '2': 'You lose'}
 
 while play[0].lower() == 'y':
@@ -9,5 +10,5 @@ while play[0].lower() == 'y':
     computer_choice = random.randint(0, 2)
     print("Your play is {0}. \nThe computer' play is {1}. \n{2}".format(options[user_input], options[computer_choice], result[str(user_input-computer_choice)]))
     play = input('Do you want to play? (y/n)')
-
+#end while
 print('Program ends')
