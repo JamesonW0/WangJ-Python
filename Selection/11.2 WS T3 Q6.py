cb119e4b906911c59cf2b1d2b8bafb6577bec216
JamesonW0 <51969225+JamesonW0@users.noreply.github.com>
2021-10-13 16:45:06 +0100

    Update 11.2 WS T3 Q6.py

--- Selection/11.2 WS T3 Q6.py
+++ Selection/11.2 WS T3 Q6.py
@@ -5,21 +5,18 @@ try:
 except ValueError:
     print('Invalid entry')
 else:
-    while True: ### SRC - I'm not sure why you need this and the break/continues? What are you trying to achieve?
+    while True:  # SRC - I'm not sure why you need this and the break/continues? What are you trying to achieve?
+        # if the user entered no, then the program will ask 'proceed or cancel' again, until the user enters yes.
         p_c = str(input('Proceed or cancel?'))
         if p_c[0].lower() == 'p':
             if str(input('Do you want to proceed? (y/n)'))[0].lower() == 'y':
                 print('PROCEED. Have a nice day')
                 break
-            else:
-                continue
             # end if
         else:
             if str(input('Do you want to cancel? (y/n)'))[0].lower() == 'y':
                 print('CANCEL. Have a nice day')
                 break
-            else:
-                continue
             # end if
         # end if
     # end while
