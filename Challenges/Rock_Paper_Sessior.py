cd9c2d4d864cc382c873664b045f1c9ade04ec80
JamesonW0 <51969225+JamesonW0@users.noreply.github.com>
2021-10-13 16:41:08 +0100

    Update Rock_Paper_Sessior.py

--- Challenges/Rock_Paper_Sessior.py
+++ Challenges/Rock_Paper_Sessior.py
@@ -8,7 +8,8 @@ result = {'-2': 'You win', '-1': 'You lose', '0': 'Draw', '1': 'You win', '2': '
 while play[0].lower() == 'y':
     user_input = int(input('Please enter a number? \n 1. ROCK \n 2. PAPER \n 3. SCISSOR \n')) - 1
     computer_choice = random.randint(0, 2)
-    print("Your play is {0}. \nThe computer' play is {1}. \n{2}".format(options[user_input], options[computer_choice], result[str(user_input-computer_choice)]))
+    print("Your play is {0}. \nThe computer' play is {1}. \n{2}".format(options[user_input], options[computer_choice],
+                                                                        result[str(user_input - computer_choice)]))
     play = input('Do you want to play? (y/n)')
-#end while
+# end while
 print('Program ends')
