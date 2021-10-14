3b043a1584f2518831b9f3460f1748efefd8a631
JamesonW0 <51969225+JamesonW0@users.noreply.github.com>
2021-10-14 18:26:45 +0100

    21/09/21 1

--- Iteration/H3Q1.py
+++ Iteration/H3Q1.py
@@ -4,6 +4,10 @@ for i in range(5):
     entry = input('letter')
     if ord(entry) > ord(Largest):
         Largest = entry
+    # end if
     if ord(entry) < ord(Smallest):
         Smallest = entry
+    # end if
+# next i
+
 print(Largest, Smallest)
--- Iteration/H3Q2.py
+++ Iteration/H3Q2.py
@@ -1,7 +1,10 @@
 password = 'Tues1212'
-for i in range(0,3):
+for i in range(0, 3):
     entry = input('enter password')
     if password == entry:
         print('Password accepted')
+        break
     else:
         print('Password rejected')
+    # end if
+# next i
